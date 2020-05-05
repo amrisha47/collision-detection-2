72daafb032c8e5954c04b965f4316dea03c9503a
amrisha47 <64591534+amrisha47@users.noreply.github.com>
2020-05-05 17:42:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,30 +1,26 @@
-var fixedRect, movingRect
+var fixedRect, movingRect;
 
 function setup() {
   createCanvas(1200,800);
-  fixedRect = createSprite(400, 200, 50, 50);
-  movingRect = createSprite(600, 300, 30, 50);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
 
-  fixedRect.shapeColor="green";
-  movingRect.shapeColor= "green";
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(0);
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+  background(0,0,0); 
   
-  if(fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2
-    && movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2
-    &&fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2
-    && movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2) {
-    fixedRect.shapeColor = "red";
-    movingRect.shapeColor = "red";
-  }
-  else{
-    fixedRect.shapeColor="green";
-  movingRect.shapeColor= "green";
-  }
+  
+
+ 
+ bounceOff(movingRect,fixedRect); 
 
   drawSprites();
 }
+
